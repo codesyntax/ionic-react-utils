7e713afdcb7d50e8858909b658c3a96890174735
bipoza <bipoza@users.noreply.github.com>
2024-05-27 15:34:14 +0000

    deploy: 6f5088b5e94b18a8b055616d616ac2b71e64432d

--- assets/keyboard2-legacy-BfXyRmGj.js
+++ assets/keyboard2-legacy-BfXyRmGj.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D2KLqp_R.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-9yQJ-lKL.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
