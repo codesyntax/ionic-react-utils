c210592c7f45a370970619c4f8624a6f515f6ee1
bipoza <bipoza@users.noreply.github.com>
2024-05-27 15:43:33 +0000

    deploy: c7018173813b64839d9fbbd5bb40d2f28a8b8aa3

--- assets/ios.transition-legacy-BWJhYiG8.js
+++ assets/ios.transition-legacy-BWJhYiG8.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-9yQJ-lKL.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-B5Eveu53.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
